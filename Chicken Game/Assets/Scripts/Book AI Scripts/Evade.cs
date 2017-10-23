8026b092a1e3fb4ad2f3f6b1db10d4c549865568
Wade Edwards <wade.edwards11@gmail.com>
2017-10-23 03:21:23 -0600

    AI exploration

---
+++ Chicken Game/Assets/Scripts/Book AI Scripts/Evade.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evade : Flee {
+
+	public float maxPrediciton;
+	private GameObject targetAux;
+	private Agent targetAgent;
+
+	//Implement the Awake function in order to set up 
+	//everything according to the real target
+	public override void Awake ()
+	{
+		base.Awake();
+		targetAgent = target.GetComponent<Agent>();
+		targetAux = target;
+		target = new GameObject();
+	}
+
+	public override Steering GetSteering()
+	{
+		Vector3 direction = targetAux.transform.position - transform.position;
+		float distance = direction.magnitude;
+		float speed = agent.velocity.magnitude;
+		float prediction;
+		if (speed <= distance / maxPrediciton)
+			prediction = maxPrediciton;
+		else 
+			prediction = distance / speed;
+		
+		target.transform.position = targetAux.transform.position;
+		target.transform.position += targetAgent.velocity * prediction;
+		return base.GetSteering();
+	}
+
+	// implement the OnDestroy function, to properly handle the internal object
+	void OnDestroy ()
+	{
+		Destroy(targetAux);
+	}
+}
---
+++ Chicken Game/Assets/Scripts/ChickenFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFollow : MonoBehaviour {
+
+	public Transform target;
+	public Transform myTransform;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * Time.deltaTime);
+	}
+}
