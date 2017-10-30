396740d43d7adae20722c85e6c969cb23b9fc52d
Wade Edwards <wade.edwards11@gmail.com>
2017-10-30 02:39:17 -0600

    Chicken Shit

--- Chicken Game/Assets/Scripts/RunAway.cs
+++ Chicken Game/Assets/Scripts/RunAway.cs
@@ -16,10 +16,14 @@ public class RunAway : MonoBehaviour {
 	public Transform Wolf;
 	public Transform PenLocation;
 
+	[SerializeField]
+	Transform destination;
+	UnityEngine.AI.NavMeshAgent navMeshAgent;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
 	}
 	
 	void OnTriggerStay(Collider other)
@@ -38,26 +42,20 @@ public class RunAway : MonoBehaviour {
 		}
 		else
 		{
-			StartCoroutine (Wander());
-			// if (isRunning == 1)
-			// {
-			// 	StartCoroutine (Wander());
-			// }
-			//transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+			SetDestination();
 		}
 	}
 
-	public IEnumerator Wander()
+	private void SetDestination()
 	{
-		//isRunning = 0;
-		randNum = Random.Range(0.0f, 10.0f);
-		yield return new WaitForSeconds(numberOfSeconds);
-		gameObject.transform.Translate(randNum * Time.deltaTime, 0, 0);
-		
-		//gameObject.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
-		
-		//isRunning = 1;
+		if(destination !=null)
+		{
+			Vector3 targetVector = destination.transform.position;
+			navMeshAgent.SetDestination(targetVector);
+		}
 	}
+
+	
 
 	void OnCollisionEnter(Collision other)
 	{
@@ -73,79 +71,21 @@ public class RunAway : MonoBehaviour {
 	}
 }
 
-// public class RunAway : MonoBehaviour {
+		//StartCoroutine (Wander());
+			// if (isRunning == 1)
+			// {
+			// 	StartCoroutine (Wander());
+			// }
+			//transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
 
-// 	// Use this for initialization
-// 	public Rigidbody enemy;
-
-// 	public float randNum;
-// 	public float moveSpeed;
-// 	public int isRunning = 1;
-// 	public int numberOfSeconds;
-
-// 	public Transform Player;
-// 	public Transform Wolf;
-// 	public Transform PenLocation;
-
-
-// 	// Use this for initialization
-// 	void Start () 
-// 	{
-// 		StartCoroutine (Wander());
-// 	}
-	
-	
-			
-// 			// if (isRunning == 1)
-// 			// {
-// 			// 	StartCoroutine (Wander());
-// 			// }
-// 			//transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
-// 	void OnCollisionEnter(Collision other)
-// 	{
-// 		if(other.gameObject.name == "Player")
-// 		{
-// 			//Destroy(gameObject); 
-// 			//ScoreManager.AddPoints(points);
-
-// 			transform.position = PenLocation.position;
-// 			transform.rotation = PenLocation.rotation;
-// 		}
-
-// 	}
-// 	void OnTriggerStay(Collider other)
-// 		{
-// 			if (other.gameObject.name == "Player")
-// 			{
-// 				Debug.Log("Player has entered chicken's trigger");
-// 				transform.LookAt(Player);
-// 				transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
-// 			}
-// 			else if (other.gameObject.name == "Wolf")
-// 			{
-// 				Debug.Log("Wolf has entered chicken's trigger");
-// 				transform.LookAt(Wolf);
-// 				transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
-// 			}
-// 			else
-// 			{
-// 				//isRunning = 0;
-// 				randNum = Random.Range(0.0f, 10.0f);
-// 				yield return new WaitForSeconds(numberOfSeconds);
-// 				gameObject.transform.Translate(randNum * Time.deltaTime, 0, 0);
-
-// 			//gameObject.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);		
-// 		    //isRunning = 1;
-// 			}
-
-	
-// }
-
-// 	public IEnumerator Wander()
-// 	{
-// 		OnTriggerStay(Collider other);
-// 		OnCollisionEnter(Collision other);
-// 	}
-	
-
-
+	// 	public IEnumerator Wander()
+	// {
+	// 	//isRunning = 0;
+	// 	randNum = Random.Range(0.0f, 10.0f);
+	// 	yield return new WaitForSeconds(numberOfSeconds);
+	// 	gameObject.transform.Translate(randNum * Time.deltaTime, 0, 0);
+		
+	// 	//gameObject.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+		
+	// 	//isRunning = 1;
+	// }
