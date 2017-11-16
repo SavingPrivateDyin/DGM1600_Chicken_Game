9928f522534766499a6b8127abe08a64582cb64f
Unknown <wade.edwards11@gmail.com>
2017-11-15 20:16:39 -0700

    Shooting Mechanics

---
+++ Chicken Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public int damage = 1;
+
+	public int time = 2;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyBullet());
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		var hit = other.gameObject;
+		var health = hit.GetComponent<wolfHealth>();
+
+		if(health != null)
+		{
+			health.TakeDamage(damage);
+		}
+	}
+
+	IEnumerator DestroyBullet()
+	{
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Chicken Game/Assets/Scripts/wolfHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfHealth : MonoBehaviour {
+	public int currentHealth;
+	public int maxHealth = 3;
+	public Transform spawnPoint;
+	public int points;
+	// Use this for initialization
+
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= amount;
+		if(currentHealth <= 0)
+		{
+			currentHealth = 0;
+			print("wolf id dead!");
+			ScoreManager.AddPoints(points);
+			transform.position = spawnPoint.position;
+			transform.rotation = spawnPoint.rotation;
+
+			currentHealth = maxHealth;
+		}
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
