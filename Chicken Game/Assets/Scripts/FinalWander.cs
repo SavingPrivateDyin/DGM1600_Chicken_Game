8465f6868ef1ca5073f91951e1a9599eb64f886f
Wade Edwards <wade.edwards11@gmail.com>
2017-12-06 08:58:13 -0700

    UPDATES and Fixes to Chicken Wander Script

--- Chicken Game/Assets/Scripts/FinalWander.cs
+++ Chicken Game/Assets/Scripts/FinalWander.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class FinalWander : MonoBehaviour {
 	
-	public float speed = 10.0f;
+	public float moveSpeed = 10.0f;
 	public float rotSpeed = 60.0f;
 	Vector3 turnAround = new Vector3(0,1,0);
+	public int points = 10;
+	public Transform Player;
+	public Transform Wolf;
+	public Transform PenLocation;
+	public Collider other;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,11 +19,15 @@ public class FinalWander : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Wander();
+		OnTriggerStay(other);
+		// if(OnTriggerStay(Collider other) == false)
+		// {
+		// 	Wander();
+		// }
 	}
 
 	void Wander(){
-		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 	      
 		
 	}
@@ -31,6 +40,36 @@ public class FinalWander : MonoBehaviour {
 			transform.Rotate(turnAround * rotSpeed * Time.deltaTime);
 					
 		}
+		else if (other.gameObject.name == "Player")
+		{
+			Debug.Log("Player has entered chicken's trigger");
+			transform.LookAt(Player);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+		else if (other.gameObject.name == "Wolf")
+		{
+			Debug.Log("Wolf has entered chicken's trigger");
+			transform.LookAt(Wolf);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+		// else{
+		// 	StartCoroutine(Wander);
+		// 	transform.Rotate(0,Random.Range(0,360),0);
+		// 	transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		// 	// Wander();
+		// }
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
 		
+		if(other.gameObject.name == "Player")
+		{
+			//Destroy(gameObject); 
+			ScoreManager.AddPoints(points);
+			transform.position = PenLocation.position;
+			transform.rotation = PenLocation.rotation;
+		}
+
 	}
 }
