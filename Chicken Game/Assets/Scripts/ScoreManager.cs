946183605fbe211cab2d21b1b904942c90e68982
Unknown <wade.edwards11@gmail.com>
2017-12-06 20:19:45 -0700

    Chicken Win Script finished

--- Chicken Game/Assets/Scripts/ScoreManager.cs
+++ Chicken Game/Assets/Scripts/ScoreManager.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class ScoreManager : MonoBehaviour {
 
 	public static int score;
-	public int scoreAmount = score;
-
+	public Text winText;
+	public int winScore;
 	public Text text;
 
 	// Use this for initialization
 	void Start () {
+		winText.GetComponent<Text>().enabled = false;
 		text = GetComponent<Text>();
 		   score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(score < 0)
+		if(score < 0){
 			score = 0;
 
 		text.text = " " + score;
+		}
+		//if the player wins display win text
+		if( winScore == score )
+		{
+			print("win score reached dummy " + score);
+			winText.GetComponent<Text>().enabled = true; 
+			Time.timeScale = 0;
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
 	}
 
 	public static void AddPoints(int pointsToAdd){
--- Chicken Game/Assets/Scripts/Win.cs
+++ Chicken Game/Assets/Scripts/Win.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class Win : MonoBehaviour {
+// public class Win : MonoBehaviour {
 
-	public int winScore; 
-	public GameObject scoreManager;
-	int currentScore;
+// 	public int winScore; 
+// 	public GameObject scoreManager;
+// 	int currentScore;
 
-	public Text winText;
+// 	public Text winText;
 
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 		
-		winText.GetComponent<Text>().enabled = false;
-	}
+// 		winText.GetComponent<Text>().enabled = false;
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
+// 	// Update is called once per frame
+// 	void Update () {
 		
-		currentScore = scoreManager.gameObject.GetComponent<ScoreManager>().scoreAmount;
-		print("current Score= " + currentScore);
-		if( currentScore == winScore )
-		{
-			print("win score reached dummy" + currentScore);
-			winText.GetComponent<Text>().enabled = true; 
-		}
-	}
-}
+// 		currentScore = scoreManager.gameObject.GetComponent<ScoreManager>().scoreAmount;
+// 		print("current Score= " + currentScore);
+// 		if( currentScore == winScore )
+// 		{
+// 			print("win score reached dummy " + currentScore);
+// 			winText.GetComponent<Text>().enabled = true; 
+// 		}
+// 	}
+// }
